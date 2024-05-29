32e82ad28a1d2fb1f4ca08f47f2e8fe819cd1bbc
Teo385 <teo3851@gmail.comgit config --global user.email teo3851@gmail.com>
2024-05-29 11:26:21 -0500

    Avance en la pages home y labs tanto en el html como en le ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import {Task} from './../../models/task.model'
+import{ FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+// ahora task es ua interfaz que me dice la estructura para poder agregar algo a el array
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: `crear proyecto` ,
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: `crear componentes `,
+      completed: false,
+    }
+  ]);
+
+  // manejar y darle control a la entrada de valores
+  newTaskCtrl = new FormControl('',{
+    nonNullable: true,
+    validators: [
+      Validators.required,
+    ]
+  }
+    
+  )
+
+  changeHanler(){
+    if(this.newTaskCtrl.valid){
+      const value = this.newTaskCtrl.value
+      this.addTask(value);
+      this.newTaskCtrl.setValue('')
+    }
+  }
+
+  // changeHanler(event: Event){
+  //   const input = event.target as HTMLInputElement;
+  //   const newTask = input.value;
+  //   this.addTask(newTask)
+  // }
+
+  addTask(title: string){
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false
+    };
+    this.tasks.update((tasks)=> [...tasks, newTask]);
+  }
+
+  deleteTalks(index: number){
+    this.tasks.update((tasks) => tasks.filter((task, position)=> position !== index))
+
+  }
+// es basado en actulizar una pocision en especifico sin mutar el array
+  updateTaks(index: number){
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) =>{
+        if (position === index){
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    })
+
+
+  }
+
+
 
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,37 +1,85 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import{ FormControl, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
+
   Welcome = 'Hola!';
-  tasks = [
+  tasks = signal([
     'Instalar el Angular CLI',
     'Crear proyecto',
     'Crear componentes'
-  ]
-  name = "Mateo";
+  ])
+  // un signal es para que saber que hubo un cambio y ya con esto se puede actualizar o  hacer una reaccion en cadena con ese dato (señal)
+  name = signal("Mateo");
   age = 23;
   disabled = true;
   img = 'https://w3schools.com/howto/img_avatar.png';
-  person ={
+
+  person =signal({
     name: 'Teo',
     age: 23,
     avartar:"https://w3schools.com/howto/img_avatar.png"
+  });
+
+  colorCtrl = new FormControl();
+  // esta es la manera de ver el cambio pero desde la logica
+  constructor(){
+    this.colorCtrl.valueChanges.subscribe(value =>{
+      console.log(value);
+    })
   }
 
   clickHandler(){
     alert("Hola Teo")
   }
 
+  // para ver los cambios 
   changeHanler(event: Event){
-    console.log(event)
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
   }
+
+  keydownHanler(event: KeyboardEvent ){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
+
+  changeAge(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update(prevState =>{
+      return{
+        ...prevState,
+        age: parseInt(newValue,10)
+      }
+    });
+  }
+
+
+
+  changeName(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update(prevState =>{
+      return{
+        ...prevState,
+        name: newValue
+      }
+    });
+  }
+
+  
+
+
 
 
 
